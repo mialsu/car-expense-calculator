86dcad1e3da2c28014378c454f06327496eddaf4
Miska Sulander <mialsu@utu.fi>
2022-01-20 14:13:41 +0200

    update

--- frontend/src/components/DataForm.js
+++ frontend/src/components/DataForm.js
@@ -1,35 +1,47 @@
 const DataForm = () => {
+
+    var maxYear = new Date().getFullYear();
+
     return(
+        
         <form className = "submit-form">
             <div className = 'form-control'>
                 <label>Ajoneuvotyyppi</label>
                 <select id = 'vehicle-type'>
-                    <option value='henkiloauto' selected>Henkilöauto</option>
+                    <option value='henkiloauto'>Henkilöauto</option>
                     <option value='pakettiauto'>Pakettiauto</option>
                 </select>
             </div>
             <div className = 'form-control'>
+                <label>Käyttönottovuosi</label>
+                <input type='number' min='1970' max = {maxYear} step='1' placeholder='' />
+            </div>
+            <div className = 'form-control'>
+                <label>Kokonaismassa</label>
+                <input type='number' min='0' max = '3500' step='1' placeholder='kg' />
+            </div>
+            <div className = 'form-control'>
                 <label>Käyttövoima</label>
                 <select id = 'motor-power'>
-                    <option value='gasoline' selected>Bensiini</option>
+                    <option value='gasoline'>Bensiini</option>
                     <option value='diesel'>Diesel</option>
                 </select>
             </div>
             <div className = 'form-control'>
                 <label>Ajoneuvon CO2 päästöt</label>
-                <input type='number' min='0'step='1' placeholder='Syötä ajoneuvon CO2 päästöt' />
+                <input type='number' min='0'step='1' placeholder='g/100km' />
             </div>
             <div className = 'form-control'>
                 <label>Ajoneuvon keskikulutus</label>
-                <input type='number' min='0' step='0.1' placeholder='Syötä ajoneuvon keskikulutus' />
+                <input type='number' min='0' step='0.1' placeholder='l/100km' />
             </div>
             <div className = 'form-control'>
                 <label>Vuosittaiset ajokilometrit</label>
-                <input type='number' min='0' placeholder='Syötä vuosittaiset ajokilometrit' />
+                <input type='number' min='0' placeholder='km'/>
             </div>
             <div className = 'form-control'>
                 <label>Vakuutuksen vuosimaksu</label>
-                <input type='number' min='0' step='0.01' placeholder='Syötä ajoneuvovakuutuksen vuosimaksu' />
+                <input type='number' min='0' step='0.01' placeholder='€' />
             </div>
             <input type='button' value ='Laske kustannukset' />
         </form>
