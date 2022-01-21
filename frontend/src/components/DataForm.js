1adb3e5e58fd38068a5db76db80b42266bc400ae
Miska Sulander <mialsu@utu.fi>
2022-01-21 11:13:26 +0200

    update

--- frontend/src/components/DataForm.js
+++ frontend/src/components/DataForm.js
@@ -1,52 +1,64 @@
+import React from 'react'
 
-const DataForm = () => {
 
-    var maxYear = new Date().getFullYear();
 
-    return(
-        
-        <form className = "submit-form" action="#" method = "post">
-            <div className = 'form-control'>
-                <label>Ajoneuvotyyppi</label>
-                <select id = 'vehicle-type' name = 'vehicle-type'>
-                    <option value='henkiloauto'>Henkilöauto</option>
-                    <option value='pakettiauto'>Pakettiauto</option>
-                </select>
-            </div>
-            <div className = 'form-control'>
-                <label>Käyttönottovuosi</label>
-                <input type='number' min='1970' max = {maxYear} step='1' placeholder='' name = 'year-registered'/>
-            </div>
-            <div className = 'form-control'>
-                <label>Kokonaismassa</label>
-                <input type='number' min='0' max = '3500' step='1' placeholder='kg' name = 'mass'/>
-            </div>
-            <div className = 'form-control'>
-                <label>Käyttövoima</label>
-                <select id = 'motor-power' name = 'motor-power'>
-                    <option value='gasoline'>Bensiini</option>
-                    <option value='diesel'>Diesel</option>
-                </select>
-            </div>
-            <div className = 'form-control'>
-                <label>Ajoneuvon CO2 päästöt</label>
-                <input type='number' min='0'step='1' placeholder='g/100km' name = 'emissions'/>
-            </div>
-            <div className = 'form-control'>
-                <label>Ajoneuvon keskikulutus</label>
-                <input type='number' min='0' step='0.1' placeholder='l/100km' name = 'consumption'/>
-            </div>
-            <div className = 'form-control'>
-                <label>Vuosittaiset ajokilometrit</label>
-                <input type='number' min='0' placeholder='km' name = 'yearly-kilometers'/>
-            </div>
-            <div className = 'form-control'>
-                <label>Vakuutuksen vuosimaksu</label>
-                <input type='number' min='0' step='0.01' placeholder='€' name = 'yearly-insurance'/>
-            </div>
-            <input type='button' value ='Laske kustannukset' />
-        </form>
-    )
+class DataForm extends React.Component {
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(event.target.vehicletype.value)
+        console.log(event.target.yearregistered.value)
+    }
+    
+    render() {
+
+        var maxYear = new Date().getFullYear();
+
+        return (
+            
+            <form className = "submit-form" onSubmit={this.handleSubmit}>
+                <div className = 'form-control'>
+                    <label>Ajoneuvotyyppi</label>
+                    <select id = 'vehicle-type' name = 'vehicletype'>
+                        <option value='henkiloauto'>Henkilöauto</option>
+                        <option value='pakettiauto'>Pakettiauto</option>
+                    </select>
+                </div>
+                <div className = 'form-control'>
+                    <label>Käyttönottovuosi</label>
+                    <input type='number' min='1970' max = {maxYear} step='1' placeholder='' name = 'yearregistered'/>
+                </div>
+                <div className = 'form-control'>
+                    <label>Kokonaismassa</label>
+                    <input type='number' min='0' max = '3500' step='1' placeholder='kg' name = 'mass'/>
+                </div>
+                <div className = 'form-control'>
+                    <label>Käyttövoima</label>
+                    <select id = 'motor-power' name = 'motor-power'>
+                        <option value='gasoline'>Bensiini</option>
+                        <option value='diesel'>Diesel</option>
+                    </select>
+                </div>
+                <div className = 'form-control'>
+                    <label>Ajoneuvon CO2 päästöt</label>
+                    <input type='number' min='0'step='1' placeholder='g/100km' name = 'emissions'/>
+                </div>
+                <div className = 'form-control'>
+                    <label>Ajoneuvon keskikulutus</label>
+                    <input type='number' min='0' step='0.1' placeholder='l/100km' name = 'consumption'/>
+                </div>
+                <div className = 'form-control'>
+                    <label>Vuosittaiset ajokilometrit</label>
+                    <input type='number' min='0' placeholder='km' name = 'yearly-kilometers'/>
+                </div>
+                <div className = 'form-control'>
+                    <label>Vakuutuksen vuosimaksu</label>
+                    <input type='number' min='0' step='0.01' placeholder='€' name = 'yearly-insurance'/>
+                </div>
+                <button type='submit'>Laske kustannukset</button> 
+            </form>
+        )
+    };
 }
 
 export default DataForm
