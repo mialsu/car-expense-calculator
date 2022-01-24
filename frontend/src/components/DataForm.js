e2cf2ac06c77d5e67ed214d3ca2ce35328226c33
Miska Sulander <mialsu@utu.fi>
2022-01-24 13:55:13 +0200

    minor rework

--- frontend/src/components/DataForm.js
+++ frontend/src/components/DataForm.js
@@ -24,7 +24,7 @@ class DataForm extends React.Component {
 
     render() {
 
-        var maxYear = new Date().getFullYear();
+        const maxYear = new Date().getFullYear();
 
         return (
             <div>
