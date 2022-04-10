77cb142f80293677ece85535786f84b60a381ec9
Miska Sulander <miska.sulander@sarkain.fi>
2022-04-10 21:35:56 +0300

    update tests

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,15 +1,23 @@
 from rest_framework.test import APIRequestFactory
 
-# Test simple post request
-factory = APIRequestFactory()
-test_request = {
-    'vehicle_type': 'henkiloauto',
-    'year_registered': '2006',
-    'mass': '2050',
-    'motor_power': 'diesel',
-    'consumption': '5.8',
-    'yearly_kilometers': '15000',
-    'fuel_price': '2.193',
-    'yearly_insurance': '332.68'
-    }
-factory.post('calculate',test_request)
+from backend.api.views import CalculationResult
+
+def test_post(self):
+    # Test simple post request
+    factory = APIRequestFactory()
+    test_request = {
+        "vehicle-type": "henkiloauto",
+        "year-registered": "2006",
+        "mass": "2050",
+        "motor-power": "diesel",
+        "consumption": "5.8",
+        "emissions": "158",
+        "yearly-kilometers": "15000",
+        "fuel-price": "2.193",
+        "yearly-insurance": "332.68"
+        }
+    view = CalculationResult.as_view()
+    request = factory.post('calculate',test_request)
+    response = self.view(request)
+    response.render()
+    self.assertEqual(response.content, {"yearly": 2880.44, "monthly": 240.04})
