b94a8bb672c1d4da511bd96e233ecf64d15e6e54
Miska Sulander <miska.sulander@sarkain.fi>
2022-04-10 20:49:55 +0300

    backend testing

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from rest_framework.test import APIRequestFactory
 
-# Create your tests here.
+# Test simple post request
+factory = APIRequestFactory()
+test_request = {
+    'vehicle_type': 'henkiloauto',
+    'year_registered': '2006',
+    'mass': '2050',
+    'motor_power': 'diesel',
+    'consumption': '5.8',
+    'yearly_kilometers': '15000',
+    'fuel_price': '2.193',
+    'yearly_insurance': '332.68'
+    }
+factory.post('calculate',test_request)
