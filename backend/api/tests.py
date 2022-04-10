9e31fd2b763ffebd0509ae2bf971caa635d6788f
Miska Sulander <miska.sulander@sarkain.fi>
2022-04-10 22:44:27 +0300

    updated tests.py

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,23 +1,31 @@
-from rest_framework.test import APIRequestFactory
+from rest_framework.test import APITestCase
+from rest_framework.test import APIClient
+from rest_framework import status
 
-from backend.api.views import CalculationResult
+from .views import CalculationResult
+from .models import TaxClass
 
-def test_post(self):
-    # Test simple post request
-    factory = APIRequestFactory()
-    test_request = {
-        "vehicle-type": "henkiloauto",
-        "year-registered": "2006",
-        "mass": "2050",
-        "motor-power": "diesel",
-        "consumption": "5.8",
-        "emissions": "158",
-        "yearly-kilometers": "15000",
-        "fuel-price": "2.193",
-        "yearly-insurance": "332.68"
-        }
-    view = CalculationResult.as_view()
-    request = factory.post('calculate',test_request)
-    response = self.view(request)
-    response.render()
-    self.assertEqual(response.content, {"yearly": 2880.44, "monthly": 240.04})
+class CalculationTest(APITestCase):
+    client = APIClient()
+    def setUp(self):
+        self.tax_class = TaxClass.objects.create(tax_class=158, yearly_tax=218.27, tax_norm = "nedc")
+        self.tax_class.save()
+
+    def test_post(self):
+        '''Test if simple post request is OK.'''
+        test_request = {
+            "vehicle-type": "henkiloauto",
+            "year-registered": "2006",
+            "mass": "2050",
+            "motor-power": "diesel",
+            "consumption": "5.8",
+            "emissions": "158",
+            "yearly-kilometers": "15000",
+            "fuel-price": "2.193",
+            "yearly-insurance": "332.68"
+            }
+        response = self.client.post('/api/calculate/', test_request, format='json')
+        #print("Vastaus on: ")
+        #print(response.content.decode("UTF-8"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        #self.assertEqual(response.content.decode("UTF-8"), {"yearly": 2880.44, "monthly": 240.04})
